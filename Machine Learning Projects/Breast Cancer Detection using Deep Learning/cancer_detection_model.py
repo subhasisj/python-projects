8442efec0d3352ecf832f725a04abab5e7474a1f
Jethy <subhasis.jethy01@sap.com>
2019-09-27 23:15:32 +0530

    Prediction + Accuracy

--- Machine Learning Projects/Breast Cancer Detection using Deep Learning/cancer_detection_model.py
+++ Machine Learning Projects/Breast Cancer Detection using Deep Learning/cancer_detection_model.py
@@ -22,45 +22,48 @@ class CancerModel:
 #               Can perform better than standard convolution in some situations.        
             
 #               Create the layers
-            model.add(SeparableConv2D(filters=32,kernel_size=(3,3),padding='same',input_shape = image_shape ))
-            model.add(Activation('relu'))
-            model.add(BatchNormalization(axis = chanDim))
-            model.add(MaxPooling2D(pool_size=(2,2)))
-            model.add(Dropout(0.25))
+        model.add(SeparableConv2D(filters=32,kernel_size=(3,3),padding='same',input_shape = image_shape ))
+        model.add(Activation('relu'))
+        model.add(BatchNormalization(axis = chanDim))
+        model.add(MaxPooling2D(pool_size=(2,2)))
+        model.add(Dropout(0.25))
+        
+        model.add(SeparableConv2D(filters=64,kernel_size=(3,3),padding='same',input_shape = image_shape ))
+        model.add(Activation('relu'))
+        model.add(BatchNormalization(axis = chanDim))
+        model.add(SeparableConv2D(filters=64,kernel_size=(3,3),padding='same',input_shape = image_shape ))
+        model.add(Activation('relu'))
+        model.add(BatchNormalization(axis = chanDim))
+        model.add(MaxPooling2D(pool_size=(2,2)))
+        model.add(Dropout(0.25))
+        
+        model.add(SeparableConv2D(filters=128,kernel_size=(3,3),padding='same',input_shape = image_shape ))
+        model.add(Activation('relu'))
+        model.add(BatchNormalization(axis = chanDim))
+        model.add(SeparableConv2D(filters=128,kernel_size=(3,3),padding='same',input_shape = image_shape ))
+        model.add(Activation('relu'))
+        model.add(BatchNormalization(axis = chanDim))
+        model.add(SeparableConv2D(filters=128,kernel_size=(3,3),padding='same',input_shape = image_shape ))
+        model.add(Activation('relu'))
+        model.add(BatchNormalization(axis = chanDim))
+        model.add(MaxPooling2D(pool_size=(2,2)))
+        model.add(Dropout(0.25))
             
-            model.add(SeparableConv2D(filters=64,kernel_size=(3,3),padding='same',input_shape = image_shape ))
-            model.add(Activation('relu'))
-            model.add(BatchNormalization(axis = chanDim))
-            model.add(SeparableConv2D(filters=64,kernel_size=(3,3),padding='same',input_shape = image_shape ))
-            model.add(Activation('relu'))
-            model.add(BatchNormalization(axis = chanDim))
-            model.add(MaxPooling2D(pool_size=(2,2)))
-            model.add(Dropout(0.25))
+#       Fully connected layers
+        
+        model.add(Flatten())
+        model.add(Dense(256))
+        model.add(Activation('relu'))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.5))
+        
+        model.add(Dense(classes))
+        model.add(Activation('softmax'))
+        
+        
+        return model
+        
             
-            model.add(SeparableConv2D(filters=128,kernel_size=(3,3),padding='same',input_shape = image_shape ))
-            model.add(Activation('relu'))
-            model.add(BatchNormalization(axis = chanDim))
-            model.add(SeparableConv2D(filters=128,kernel_size=(3,3),padding='same',input_shape = image_shape ))
-            model.add(Activation('relu'))
-            model.add(BatchNormalization(axis = chanDim))
-            model.add(SeparableConv2D(filters=128,kernel_size=(3,3),padding='same',input_shape = image_shape ))
-            model.add(Activation('relu'))
-            model.add(BatchNormalization(axis = chanDim))
-            model.add(MaxPooling2D(pool_size=(2,2)))
-            model.add(Dropout(0.25))
-            
-#            Fully connected layers
-            
-            model.add(Flatten())
-            model.add(Dense(256))
-            model.add(Activation('relu'))
-            model.add(BatchNormalization())
-            model.add(Dropout(0.5))
-            
-            model.add(Dense(classes))
-            model.add('softmax')
-            
-            return model
-            
-            
-            
+
+model = CancerModel.build((24,34,3),2)
+model.summary()
