d9e7a3ef684f78ad06ea256fff32fda504967ab8
Subhasis Jethy <46371699+subhasisj@users.noreply.github.com>
2022-02-24 00:36:49 +0530

    Addition/Concat

---
+++ Exercises/for-ladi/01. The-Beginning.py
@@ -0,0 +1,18 @@
+
+def add_two_numbers(a, b):
+    return a % b
+
+def check_what_happens(value1, value2):
+    return value1 * value2
+
+if __name__ == '__main__':
+    # print(add_two_numbers(7, 2))
+    # print(check_what_happens(5, 'Ladi'))
+    ladi = "xyz"
+    ladi = 5
+    ladi = 'xyz'
+    ladi = 5
+
+    print(ladi)
+
+
